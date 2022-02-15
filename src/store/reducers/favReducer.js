74ac3175ba6912999ef9325ddc13947e87bf6fca
SiamakAlipour <siamakalipour37@gmail.com>
2022-02-15 19:36:28 +0330

    fav reducer changes + fix

--- src/store/reducers/favReducer.js
+++ src/store/reducers/favReducer.js
@@ -1,17 +1,19 @@
 import { getCookie, setCookie } from '../../service/cookies'
 import { FAV_ADD, FAV_GET, FAV_REMOVE } from '../actions/types'
-const initialState = JSON.parse(getCookie('fav'))
+const initialState = getCookie('fav') ? getCookie('fav') : []
+
 const favReducer = (state = initialState, action) => {
 	const { type, payload } = action
 	switch (type) {
 		case FAV_ADD:
-			return setCookie('fav', JSON.stringify([...state, payload.value]))
+			setCookie('fav', [...state, payload.value])
+			return [...state, payload.value]
 
 		case FAV_GET:
 			return state
 		case FAV_REMOVE:
-			state.filter((item) => item.id !== payload.id)
-			return setCookie('fav', JSON.stringify([...state]))
+			return state.filter((item) => item.id !== payload.id)
+		// return setCookie('fav', [...state]))
 		default:
 			return state
 	}
