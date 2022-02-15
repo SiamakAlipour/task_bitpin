0cc476af46b98189298784bf205b78f34a3ff005
SiamakAlipour <siamakalipour37@gmail.com>
2022-02-15 19:35:45 +0330

    redux provider added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import reportWebVitals from './reportWebVitals'
-
+import store from './store'
+import { Provider } from 'react-redux'
 ReactDOM.render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
