9b8789946a5cf6cdbf82e3b4ea4866f97d6ba528
SiamakAlipour <siamakalipour37@gmail.com>
2022-02-15 19:36:59 +0330

    like with adding to cookies added

--- src/components/MarketCard.js
+++ src/components/MarketCard.js
@@ -4,8 +4,28 @@ import StarBorderIcon from '@mui/icons-material/StarBorder'
 import StarIcon from '@mui/icons-material/Star'
 import { IconButton } from '@mui/material'
 import { Link } from 'react-router-dom'
-function MarketCard({ code, price, price_info, title, title_fa, tradable }) {
+import { useDispatch, useSelector } from 'react-redux'
+import { addFav, removeFav } from '../store/actions/fav'
+import { getCookie } from '../service/cookies'
+function MarketCard({
+	id,
+	code,
+	price,
+	price_info,
+	title,
+	title_fa,
+	tradable,
+}) {
 	const [liked, setLiked] = useState(false)
+	const dispatch = useDispatch()
+	const handleLike = () => {
+		setLiked(true)
+		dispatch(addFav({ code, price, price_info, title, title_fa, tradable }))
+	}
+	const handleDisLike = () => {
+		setLiked(false)
+		dispatch(removeFav(id))
+	}
 	return (
 		<div className='marketCard'>
 			<div className='marketCard__title'>
@@ -19,11 +39,11 @@ function MarketCard({ code, price, price_info, title, title_fa, tradable }) {
 					<Link to={`/markets/${code}`}>لیست معاملات</Link>
 				</p>
 				{liked ? (
-					<IconButton color='warning' onClick={() => setLiked(false)}>
+					<IconButton color='warning' onClick={handleDisLike}>
 						<StarIcon />
 					</IconButton>
 				) : (
-					<IconButton color='primary' onClick={() => setLiked(true)}>
+					<IconButton color='primary' onClick={handleLike}>
 						<StarBorderIcon />
 					</IconButton>
 				)}
--- src/components/Markets.js
+++ src/components/Markets.js
@@ -4,7 +4,12 @@ import axios from '../service/axios'
 import MarketCard from './MarketCard'
 import { Audio } from 'react-loader-spinner'
 import Pagination from '@mui/material/Pagination'
+import { getCookie } from '../service/cookies'
+import { useDispatch, useSelector } from 'react-redux'
+import { getFav } from '../store/actions/fav'
 function Markets() {
+	const dispatch = useDispatch()
+	const fav = useSelector((state) => state.fav)
 	const [markets, setMarkets] = useState([])
 	const [loading, setLoading] = useState(true)
 	const [currentPage, setCurrentPage] = useState(1)
@@ -30,6 +35,10 @@ function Markets() {
 	const currentPosts = markets?.slice(indexOfFirstPost, indexOfLastPost)
 	// metrial ui pagination func
 	const paginate = (event, value) => setCurrentPage(value)
+	React.useEffect(() => {
+		dispatch(getFav())
+		console.log('hello', fav)
+	}, [])
 	return (
 		<div className='container markets'>
 			{loading ? (
@@ -41,6 +50,7 @@ function Markets() {
 						{currentPosts.map((market) => (
 							<MarketCard
 								key={market.id}
+								id={market.id}
 								code={market.code}
 								price={market.price}
 								price_info={market.price_info}
