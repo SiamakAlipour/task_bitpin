b24ee4e119a016f3466d35a461d89db5eaf073d2
SiamakAlipour <siamakalipour37@gmail.com>
2022-02-19 11:57:59 +0330

    Fav Market component done

--- src/components/FavMarkets.js
+++ src/components/FavMarkets.js
@@ -5,29 +5,39 @@ import MarketCard from './MarketCard'
 import { Audio } from 'react-loader-spinner'
 import Pagination from '@mui/material/Pagination'
 import { useDispatch, useSelector } from 'react-redux'
+import { getCookie } from '../store/actions/cookie'
+import { addFav, getFav } from '../store/actions/favMarkets'
+import { MARKET_ALL } from '../store/actions/types'
+import { allMarkets } from '../store/actions/market'
 
 function FavMarkets() {
-	const fav = useSelector((state) => state.fav)
+	const dispatch = useDispatch()
+	const cookie = useSelector((state) => state.cookie)
 	const markets = useSelector((state) => state.markets)
-	const [favLoading, setFavLoading] = useState(true)
-	const [favMarkets, setFavMarkets] = useState([])
+	const favMarkets = useSelector((state) => state.favMarkets)
+	const [favLoading, setFavLoading] = useState(false)
+	// const [favMarkets, setFavMarkets] = useState([])
 	const [currentPage, setCurrentPage] = useState(1)
 	const [postPerPage] = useState(9)
 	const indexOfLastPost = currentPage * postPerPage
 	const indexOfFirstPost = indexOfLastPost - postPerPage
 	const currentPosts = favMarkets?.slice(indexOfFirstPost, indexOfLastPost)
+	const paginate = (event, value) => setCurrentPage(value)
 	React.useEffect(() => {
+		dispatch(allMarkets())
+		dispatch(getCookie())
 		dispatch(getFav())
-		console.log('hello', fav)
 	}, [])
 	useEffect(() => {
-		fav.map((fav) =>
+		cookie.map((fav) =>
 			markets.find((market) => {
-				if (!favMarkets?.includes(market) && market.code === fav.code)
-					setFavMarkets([...favMarkets, market])
+				const isFound = favMarkets.some((fav) => {
+					if (fav.id === market.id) return true
+				})
+				if (isFound) return
+				else if (!isFound && market.code === fav.code) dispatch(addFav(market))
 			})
 		)
-		if (favMarkets) setFavLoading(false)
 	}, [markets])
 	return (
 		<div className='container markets'>
@@ -48,17 +58,13 @@ function FavMarkets() {
 								title_fa={market.title_fa}
 								tradable={market.tradable}
 								market={market}
-								isLiked={isLiked(market)}
+								isLiked={true}
 							/>
 						))}
 					</div>
 					<Pagination
 						color='primary'
-						count={
-							location.pathname === '/likedMarkets'
-								? Math.ceil(fav.length / postPerPage)
-								: Math.ceil(markets.length / postPerPage)
-						}
+						count={Math.ceil(favMarkets.length / postPerPage)}
 						onChange={paginate}
 					/>
 				</>
