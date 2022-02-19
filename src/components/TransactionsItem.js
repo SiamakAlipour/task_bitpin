16760348166a131629aa444143877561e40f9571
SiamakAlipour <siamakalipour37@gmail.com>
2022-02-19 17:56:51 +0330

    Transactions Item component added

---
+++ src/components/TransactionsItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './styles/TransactionsItem.scss'
+function TransactionsItem() {
+	return <div className='transactionsItem'>test</div>
+}
+
+export default TransactionsItem
