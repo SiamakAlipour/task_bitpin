bb7f3749f61ae64084e0b2f74bfb32face3295fc
SiamakAlipour <siamakalipour37@gmail.com>
2022-02-21 19:31:24 +0330

    404 page added

--- src/components/_404.js
+++ src/components/_404.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './styles/_404.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGhost } from '@fortawesome/free-solid-svg-icons'
 function _404() {
 	return (
 		<div className='_404'>
-			<h1>404</h1>
+			<h1>
+				4
+				<span>
+					<FontAwesomeIcon icon={faGhost} />{' '}
+				</span>
+				4
+			</h1>
+			<h2>Error: 404 page not found</h2>
+			<p>Sorry, the page you're looking for cannot be accessed</p>
 		</div>
 	)
 }
